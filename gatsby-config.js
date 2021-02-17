2b76da3b5cde45652723ca22e24a39fa8d575784
ElectricReality <34076729+ElectricReality@users.noreply.github.com>
2021-02-17 10:45:46 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         configPath: `src/config`,
         docsPath: `src/docs`,
         githubUrl: `https://github.com/rocketseat/gatsby-themes`,
-        baseDir: `examples/gatsby-theme-docs`,
+        baseDir: ``,
       },
     },
     {
